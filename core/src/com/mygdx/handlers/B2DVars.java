d702a4827801e3065eb4ac4d76f97405bb1c84b0
Łukasz Czerwiński <mrowacz@gmail.com>
2017-08-15 00:32:04 +0200

    finish lesson 2 : box2d lib entry
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

---
+++ core/src/com/mygdx/handlers/B2DVars.java
@@ -0,0 +1,8 @@
+package com.mygdx.handlers;
+
+/**
+ * Created by mrowacz on 15.08.17.
+ */
+public class B2DVars {
+    public static final float PPM = 100;
+}
--- core/src/com/mygdx/states/Play.java
+++ core/src/com/mygdx/states/Play.java
@@ -1,6 +1,12 @@
 package com.mygdx.states;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import static com.mygdx.handlers.B2DVars.PPM;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.mygdx.game.MyGdxGame;
 import com.mygdx.handlers.GameStateManager;
 
 /**
@@ -8,9 +14,41 @@ import com.mygdx.handlers.GameStateManager;
  */
 public class Play extends GameState {
 
-    private BitmapFont font = new BitmapFont();
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+    private OrthographicCamera b2dCam;
+
     public Play(GameStateManager gsm) {
         super(gsm);
+        world = new World(new Vector2(0, -9.81f), true);
+        b2dr = new Box2DDebugRenderer();
+
+        // create platform
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(160 / PPM, 120 / PPM);
+        bdef.type = BodyDef.BodyType.StaticBody;
+
+        Body body = world.createBody(bdef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(50 / PPM, 5 / PPM);
+        FixtureDef fdef = new FixtureDef();
+        fdef.shape = shape;
+        body.createFixture(fdef);
+
+        // creating falling box
+        bdef.position.set(160 / PPM, 200 / PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+        shape.setAsBox(5 / PPM, 5 / PPM);
+        fdef.shape = shape;
+
+        fdef.restitution = 0f;
+        body.createFixture(fdef);
+
+        // set up box2d cam;
+        b2dCam = new OrthographicCamera();
+        b2dCam.setToOrtho(false, MyGdxGame.V_WIDTH / PPM, MyGdxGame.V_HEIGHT / PPM);
     }
     @Override
     public void handleInput() {
@@ -19,16 +57,14 @@ public class Play extends GameState {
 
     @Override
     public void update(float dt) {
-
+        world.step(dt, 6, 2);
     }
 
     @Override
     public void render()
     {
-        sb.setProjectionMatrix(cam.combined);
-        sb.begin();
-        font.draw(sb, "play state test", 100, 100);
-        sb.end();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        b2dr.render(world, b2dCam.combined);
     }
 
     @Override
